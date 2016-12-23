3e9a8f7ca7694cc2a322d83677e27f62acd0c1e0
KyleProutyOSU <proutyky@oregonstate.edu>
2016-12-22 23:52:14 -0800

    basic golang webapp up and running

--- main.go
+++ main.go
@@ -1,8 +1,27 @@
+//**Created by Kyle Prouty
+//**Winter Break 2016
 
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"net/http"
+	"fmt"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.template"))
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	http.HandleFunc("/", idx)
+	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("pub/"))))
+	http.ListenAndServe(":8080", nil)
+}
+
+func idx(w http.ResponseWriter, req *http.Request) {
+	tpl.ExecuteTemplate(w, "index.template", nil)
+	fmt.Println(req.URL.Path)
 }
